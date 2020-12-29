22c279585a9415d398000a1e039912ef6da2762e
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-29 11:34:16 -0600

    commit tree b88cec38d920fd9d731af88ead024cfbdc18362d

--- app.rb
+++ app.rb
@@ -13,6 +13,16 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/goodbye/:name' do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1].to_f
+    @num2 = params[:num2].to_f
+    "#{@num1 * @num2}"
+  end
   # Code your final two routes here:
 
 end
